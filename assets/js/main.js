e0f04bce1838a5a5938c0eb86da3bab342fb1f05
unknown <hunters.cainhurst@gmail.com>
2024-02-08 22:26:00 +0330

    main page complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,66 +4,91 @@ const element = document.getElementById('nav-menu');
 const searchBar = document.getElementById('search-bar')
 const openSearchBar = document.getElementById('search-btn');
 const closeSearchBar = document.getElementById('close-search-btn');
-const shop = document.getElementById('shopping')
+const shop = document.getElementById('shopping');
 
 const shopData = [
     {
-        id: 1,
-        img:"assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
-        title : "placeholder",
-        cost: "sold"
+        "id": 1,
+        "image":"./assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
+        "title" : "placeholder",
+        "cost": "sold"
     },
     {
-        id: 2,
-        img:"assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
-        title : "placeholder",
-        cost: "sold"
+        "id": 2,
+        "image":"./assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
+        "title" : "placeholder",
+        "cost": "sold"
     },
     {
-        id: 3,
-        img:"assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
-        title : "placeholder",
-        cost: "sold"
+        "id": 3,
+        "image":"./assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
+        "title" : "placeholder",
+        "cost": "sold"
     },
     {
-        id: 4,
-        img:"assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
-        title : "placeholder",
-        cost: "sold"
+        "id": 4,
+        "image":"./assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
+        "title" : "placeholder",
+        "cost": "sold"
     },
     {
-        id: 5,
-        img:"assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
-        title : "placeholder",
-        cost: "sold"
+        "id": 5,
+        "image":"./assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
+        "title" : "placeholder",
+        "cost": "sold"
     },
     {
-        id: 6,
-        img:"assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
-        title : "placeholder",
-        cost: "sold"
+        "id": 6,
+        "image": "./assets/img/008_c_d9ab8cb4-5ebc-4585-adfc-d6cd40789e89_360x.jpg",
+        "title" : "placeholder",
+        "cost": "sold"
     }
 ]
+// const renderPosts = () =>{
+// const row = document.createElement("div");
+// row.classList.add("row","justify-content-center","gap-lg-1","border-0","mb-2")
+// const card = document.createElement("div");
+// card.classList.add("card","col-lg-3","col-md-4","border-0","shopping-item");
+//     shopData.map((item) => {
+//     const cardImg = document.createElement("img")
+//     const imgSrc = `${item.image}`
+//     cardImg.classList.add("card-img-top")
+//     cardImg.src = imgSrc;
+//     const cardBody = document.createElement("div")
+//     cardBody.classList.add("card-body")
+//     cardBody.innerHTML = `
+//       <h5 class="card-title">${item.title}</h5>
+//       <p class="card-text">${item.cost}</p>
+//     `;
+//     card.appendChild(cardImg);
+//     card.appendChild(cardBody);
+//     row.appendChild(card)
+// })  
+// shop.appendChild(row)
+// }
+// renderPosts()
+
+
 
 const renderPosts = () =>{
-    const card = document.createElement("div");
-    card.classList.add("card","col-lg-3","col-md-4","border-0","shopping-item");
     shopData.map((item) => {
-    const cardImg = document.createElement("img");
-    cardImg.classList.add("card-img-top");
-    cardImg.src="${item.img}";
-    const cardBody = document.createElement("div")
-    cardBody.classList.add("card-body")
-    cardBody.innerHTML = `
-      <h5 class="card-title">${item.title}</h5>
-      <p class="card-text">${item.cost}</p>
-    `;
-    card.appendChild(cardImg);
-    card.appendChild(cardBody);
-    
+    const card = document.createElement("div")
+    card.classList.add("card","col-lg-3","col-md-4","border-0","shopping-item")
+    card.innerHTML = `
+    <img src="${item.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${item.title}</h5>
+        <p class="card-text">${item.cost}</p>
+    </div>
+    `
+    shop.appendChild(card)
 })
-shop.appendChild(card)
 }
+
+
+renderPosts()
+
+
 
 openMenu.addEventListener('click',openM);
 closeMenu.addEventListener('click',closeM);
